3780166615503529b8a4bc0970088b47e241b27e
Dario Greto <dario.greto@gmail.com>
2023-11-11 19:46:37 +0100

    Updated keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,23 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h") -- left window
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- up window
+vim.keymap.set("n", "<C-j>", "<C-w>j") -- down window
+vim.keymap.set("n", "<C-l>", "<C-w>l") -- right window
 
+-- Resize with arrows when using multiple windows
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-right>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-left>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+vim.keymap.set("n", "<tab>", ":bnext<cr>")
+vim.keymap.set("n", "<s-tab>", ":bprevious<cr>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<cr>")
+
+-- Neotree stuff
 vim.keymap.set('n', '<C-n>', ':Neotree<CR>')
 
