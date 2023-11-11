8c2657f62b469bc47debc90c362f90754e6a8e88
Dario Greto <dario.greto@gmail.com>
2023-11-11 19:11:15 +0100

    Split options and keymaps

---
+++ lua/core/options.lua
@@ -0,0 +1,13 @@
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
